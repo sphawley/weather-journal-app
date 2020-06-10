c278548f5c920668773c3b8d427d26f538d71a32
sphawley <sphawley@berkeley.edu>
2020-06-10 12:09:07 -0700

    feat: Implement bare bones weather journal functionality
    Upon clicking generate three functions will be called, linked by .then()
    
    1. getData --> will get the current temperature from the provided zip code
    2. postData --> will post the date, temp, and any feelings to the server
    3. updateData --> will display the latest journal entry to the page

--- server.js
+++ server.js
@@ -1,25 +1,55 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Middleware */
+// Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Spin up the server
-// Callback to debug
+const port = 8080;
+const listening = () => console.log(`listening on port: ${port}`);
+app.listen(port, listening);
 
-// Initialize all route with a callback function
+// Callback to debug
+app.get('/test', (request, response) => {
+  console.log('received test');
+  response.send('working');
+});
 
 // Callback function to complete GET '/all'
+const sendData = (request, response) => {
+  response.send(projectData);
+};
+
+// Initialize all route with a callback function
+app.get('/all', sendData);
+
+// Initialize add route with a callback function
+const addEntry = (request, response) => {
+  projectData.push(request.body);
+  response.send({ response: 'Entry saved.' });
+};
 
 // Post Route
+app.post('/add', addEntry);
+
+// Jest example
+const sum = (a, b) => {
+  return a + b;
+};
+module.exports = sum;
--- website/app.js
+++ website/app.js
@@ -1,17 +1,86 @@
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const d = new Date();
+const newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
+const apiKey = 'a82da9fd33ad3905f25a5cf58bce890f';
+const createWeatherEndpoint = (zipCode) => {
+  return `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
+};
 
-// Event listener to add function to existing HTML DOM element
 
-/* Function called by event listener */
-
-/* Function to GET Web API Data*/
+/* Function to GET Web API Data */
+const getCurrentWeatherByZip = (zipCode) => getData(createWeatherEndpoint(zipCode)).main.temp;
 
 /* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  console.log('Posting data');
+  console.log(data);
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  
+  try {
+    const newData = await response.json();
+    console.log('Server response from post');
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log('error in post. error: ', error);
+  }
+};
 
 /* Function to GET Project Data */
+const getData = async (url = '') => {
+  const response = await fetch(url);
+  try {
+    const data = await response.json();
+    console.log('Received data');
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log('error in get. error: ', error);
+  }
+};
+
+const updateUI = async () => {
+  const data = await getData('/all');
+  try {
+    console.log('Got all data');
+    console.log(data);
+    document.querySelector('#date').innerHTML = data[data.length -1].date;
+    document.querySelector('#temp').innerHTML = data[data.length -1].temperature;
+    document.querySelector('#content').innerHTML = data[data.length -1].userResponse;
+    return data;
+  } catch (error) {
+    console.log('error in updateUI. error: ', error);
+  }
+};
+
+/* Function called by event listener */
+const getWeatherPostDataUpdateUI = () => {
+  console.log('Step 1: getData');
+  getData(createWeatherEndpoint(document.querySelector('#zip').value)).then(function(value){
+    console.log('Step 2: postData');
+    const postPromise = postData('/add', {temperature: value.main.temp, date: newDate, userResponse: document.querySelector('#feelings').value});
+    console.log('postPromise');
+    console.log(postPromise);
+    return postPromise;
+  }).then(function(value){
+      console.log('Step 3: updateUI');
+      const updatePromise = updateUI();
+      console.log('updatePromise');
+      console.log(updatePromise);
+    }
+  );
+}
+
+// Event listener to add function to existing HTML DOM element
+document.querySelector('#generate').addEventListener('click', getWeatherPostDataUpdateUI);
