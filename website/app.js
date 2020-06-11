5fc5ef31aa2affcb5556ed68c5200643954b38db
sphawley <sphawley@berkeley.edu>
2020-06-11 16:59:21 -0700

    style: Add comments and style conform app.js

--- website/app.js
+++ website/app.js
@@ -1,17 +1,17 @@
 /* Global Variables */
 const MAX_HISTORY_LIST_LENGTH = 5;
 
-// Create a new date instance dynamically with JS
-const getCurrentDateAndTime = () => {
-  const d = new Date();
-  const newDateAndTime = d.toLocaleString();
-  return newDateAndTime
-}
-
 // Personal API Key for OpenWeatherMap API
 const apiKey = 'a82da9fd33ad3905f25a5cf58bce890f';
 const createWeatherEndpoint = (zipCode) => {
   return `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
+};
+
+// Create a new date instance dynamically with JS
+const getCurrentDateAndTime = () => {
+  const d = new Date();
+  const newDateAndTime = d.toLocaleString();
+  return newDateAndTime;
 };
 
 /* Function to POST data */
@@ -24,7 +24,7 @@ const postData = async (url = '', data = {}) => {
     },
     body: JSON.stringify(data)
   });
-  
+
   try {
     const newData = await response.json();
     return newData;
@@ -38,8 +38,8 @@ const getData = async (url = '') => {
   const response = await fetch(url);
   try {
     const data = await response.json();
-    if (data.hasOwnProperty('cod') && data.cod[0] == '4') {
-      alert("Something went wrong.\nDid you enter a valid zipcode?");
+    if (Object.prototype.hasOwnProperty.call(data, 'cod') && data.cod[0] === '4') {
+      alert('Something went wrong.\nDid you enter a valid zipcode?');
     }
     return data;
   } catch (error) {
@@ -48,10 +48,11 @@ const getData = async (url = '') => {
 };
 
 const kelvinToFahrenheit = (kelvin) => {
-  let fahrenheit = (kelvin - 273.15)*(9/5) + 32;
-  return fahrenheit
+  const fahrenheit = (kelvin - 273.15) * (9 / 5) + 32;
+  return fahrenheit;
 };
 
+/* Update the history section to contain the latest entries */
 const updateHistory = (data) => {
   data = data.slice(0, MAX_HISTORY_LIST_LENGTH);
   const ol = document.createElement('ol');
@@ -77,13 +78,14 @@ const updateHistory = (data) => {
   document.querySelector('.history').appendChild(ol);
 };
 
+/* update the most recent entry and history */
 const updateUI = async () => {
   try {
     const data = await getData('/all');
     if (data.length > 0) {
-      document.querySelector('#recentDate').innerHTML = data[data.length -1].date;
-      document.querySelector('#recentTemp').innerHTML = data[data.length -1].temperature;
-      document.querySelector('#recentContent').innerHTML = data[data.length -1].userResponse;
+      document.querySelector('#recentDate').innerHTML = data[data.length - 1].date;
+      document.querySelector('#recentTemp').innerHTML = data[data.length - 1].temperature;
+      document.querySelector('#recentContent').innerHTML = data[data.length - 1].userResponse;
       updateHistory(data.reverse().slice(1));
     }
     return data;
@@ -94,30 +96,39 @@ const updateUI = async () => {
 
 /* Function called by event listener */
 const submitForm = () => {
-  if ((document.querySelector('#zip').value != '') && (document.querySelector('#feelings').value != '')) {
+  if ((document.querySelector('#zip').value !== '') && (document.querySelector('#feelings').value !== '')) {
     getWeatherPostDataUpdateUI();
   } else {
-    alert("Zipcode and feelings are required.");
+    alert('Zipcode and feelings are required.');
   }
-}
+};
 
+/* Function that 
+1. gets weather data 
+2. posts date, temperature, and user response to the server
+3. gets stored entries from the server and updates
+the most recent entry and history sections */
 const getWeatherPostDataUpdateUI = () => {
   getData(createWeatherEndpoint(document.querySelector('#zip').value)).then((value) => {
     const postPromise = postData(
       '/add',
-      {temperature: `${kelvinToFahrenheit(value.main.temp).toFixed(1)}°F`,
-      date: getCurrentDateAndTime(), userResponse: document.querySelector('#feelings').value}
-      );
+      {
+        temperature: `${kelvinToFahrenheit(value.main.temp).toFixed(1)}°F`,
+        date: getCurrentDateAndTime(),
+        userResponse: document.querySelector('#feelings').value
+      }
+    );
     return postPromise;
   }).then(() => {
-      const updatePromise = updateUI();
-    }
+    updateUI();
+  }
   );
-}
+};
 
 // Event listener to add function to existing HTML DOM element
 document.querySelector('#generate').addEventListener('click', submitForm);
 
+// Update the page with old entries on page load
 window.addEventListener('load', () => {
   updateUI();
 });
